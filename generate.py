c1657c80076c3a72eabb5af0d9c998ba3ceb6b57
RoCry <crysheen@gmail.com>
2024-12-23 10:30:38 +0800

    init test

---
+++ generate.py
@@ -0,0 +1,26 @@
+import pathlib
+import subprocess
+import tempfile
+import os
+
+def generate_image_for_html(html_path: str, output_path: str, viewport: str):
+    subprocess.run(
+        [
+            "puppeteer",
+            "screenshot",
+            html_path,
+            output_path,
+            "--viewport",
+            viewport,
+        ],
+    )
+    return output_path
+
+def generate_all():
+    DIST_FOLDER = "dist/images"
+    os.makedirs(DIST_FOLDER, exist_ok=True)
+    output_path = generate_image_for_html("htmls/hello.html", f"{DIST_FOLDER}/hello.png", "250x122")
+    print(f"Generated {output_path}")
+
+if __name__ == "__main__":
+    generate_all()
